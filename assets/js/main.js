c9618a923d677a04134b5dcc5feb34d5f21a1e36
VanMong <levanmonghgi2019@gmail.com>
2025-03-24 14:42:25 +0700

    Update lần 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 function showDetailAction () {
-    const imgMain = document.querySelector('.product-img__main')
-    const imgExtra = document.querySelectorAll('.product-img__extra-item')
+    const imgMain = document.querySelector('.sanphamlon-img')
+    const imgExtra = document.querySelectorAll('.sanphamnho-img-item')
     
     imgExtra.forEach((item) => {
         item.onclick = () => {

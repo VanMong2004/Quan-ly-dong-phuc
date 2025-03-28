775bae731dee33a7a43926208d50f8d823088b94
VanMong <levanmonghgi2019@gmail.com>
2025-03-28 21:35:24 +0700

    update cho nhat

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,3 @@
 
-function showDetailAction () {
-    const imgMain = document.querySelector('.product-img__main')
-    const imgExtra = document.querySelectorAll('.product-img__extra-item')
-    
-    imgExtra.forEach((item) => {
-        item.onclick = () => {
-            if (imgMain && item) {
-                imgMain.src = item.src
-            }
-        }
-    })
-}
 
 
